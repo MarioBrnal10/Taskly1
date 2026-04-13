f7af955ffec520a7662e1ecfdd131e71ab522173
Mario_Brnal10 <122044139@upq.edu.mx>
2026-04-13 12:05:46 -0600

    cambio

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from routers.usuarios import routerUsuarios
 from routers.materias import routerMaterias
 from routers.tareas import routerTareas
 from routers.prioridades import routerPrioridades
+
 app = FastAPI(
     title="API Taskly",
     version="1.0"
+)
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/", tags=["Inicio"])
@@ -18,3 +32,6 @@ app.include_router(routerMaterias)
 app.include_router(routerTareas)
 app.include_router(routerPrioridades)
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
